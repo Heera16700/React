1f6c07cfd41e4eaa39552626fc6710a6f4736851
Heera16700 <2938aman@gmail.com>
2025-07-17 15:42:45 +0530

    creating new components

--- Components/Homepage.jsx
+++ Components/Homepage.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
+import Navbar from "./Navbar";
+import image1 from "../assets/Hoodies.jpeg";
 
 const Homepage = () => {
   return (
-    <div>Homepage</div>
-  )
-}
+    <div>
+      <Navbar />
+      {/* <Slider/> */}
+      <div className="Content">
+        <h2>BEST PRODUCTS</h2>
+        <div className="Features">
+          <div className="Clothing">
+            <img src={image1} className="Img_1" alt="" />
+            <img src={image1} className="Img_1" alt="" />
+            <div className="Clothing2">
+              <img src={image1} className="Img_1" alt="" />
+              <img src={image1} className="Img_1" alt="" />
+            </div>
+           
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Homepage
+export default Homepage;
--- Components/Navbar.jsx
+++ Components/Navbar.jsx
@@ -1,16 +1,26 @@
 import React from 'react'
 import { TbShoppingCartHeart } from "react-icons/tb";
+import { TbShoppingCartSearch } from "react-icons/tb";  
 const Navbar = () => {
   return (
     <div className='Navbar'>
-      <a href='#' className='Logo'><TbShoppingCartHeart />CaRt.in</a>
+      <a href='#' className=''>
+        <span className='logo'><TbShoppingCartHeart /></span>
+        <span className='brand'>
+          <span className='green'>C</span>
+          <span className='green'>A</span>
+          <span className='white'>RT.in</span>
+
+
+        </span>
+      </a>
       <a href="#">Home</a>
       <a href="#">Contact</a> 
       <a href="">Category</a>
       <a href="">Cart</a>
       <a href="">Products</a>
       <a href="">Login</a>
-      <a href="">Search</a>
+      <a href=""><TbShoppingCartSearch /></a>
 
     </div>
   )
